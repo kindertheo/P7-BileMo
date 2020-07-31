1cfa096d151979d9e04fa8c8d59d84c6b69be778
Théo <kinder.theo@gmail.com>
2020-07-31 22:43:03 +0200

    Product Part

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Client;
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        for($i= 0;$i < 10; $i++){
+            $product = new Product();
+
+            $product->setName($faker->word())
+                ->setDescription($faker->paragraph())
+                ->setPrice($faker->randomNumber(2));
+
+            $manager->persist($product);
+        }
+
+        $clients= [] ;
+
+        for($i = 0; $i < 5; $i++){
+        $client = new Client();
+        $client->setName($faker->word());
+
+        $manager->persist($client);
+
+        $clients[] = $client;
+        }
+
+        for($i = 0; $i < 20; $i++){
+            $user = new User();
+
+            $user->setName($faker->name)
+                ->setEmail($faker->email)
+                ->setRole("user")
+                ->setPassword($faker->password)
+                ->setClient($clients[random_int(0, count($clients) - 1)]);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
