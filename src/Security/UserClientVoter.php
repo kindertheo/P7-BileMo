41aaf3ffc69cb6ceb938e244efa1f889b7a29d90
Théo <kinder.theo@gmail.com>
2020-08-24 15:46:40 +0200

    create a Voter
    so user can't access to data of another clients

---
+++ src/Security/UserClientVoter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Client;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class UserClientVoter extends Voter {
+
+
+    const VIEW = 'view';
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject)
+    {
+        // if the attribute isn't one we support, return false
+        if (!in_array($attribute, (array)self::VIEW)) {
+            return false;
+        }
+
+        if(!$subject instanceof Client){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     *
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        $client = $subject;
+
+        if($this->canView($client, $user)){
+            return true;
+        }
+
+        throw new \LogicException("You can't access users from another client");
+
+    }
+
+
+    private function canView(Client $client, User $user){
+        return $user->getClient() === $client;
+    }
+}
