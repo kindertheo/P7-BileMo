c0c1bac92cbdc3ba27b2752ea980a3458c974726
Théo <kinder.theo@gmail.com>
2020-08-24 22:40:35 +0200

    correct encoding error

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener;
 
+use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
@@ -9,6 +10,13 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener
 {
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
     public function onKernelException(ExceptionEvent $event)
     {
         // You get the exception object from the received event
@@ -19,8 +27,10 @@ class ExceptionListener
             'code' => $exception->getCode()
         ];
 
+        $message = stripslashes(stripcslashes($this->serializer->serialize($message, "json")));
+
         // Customize your response object to display the exception details
-        $response = new JsonResponse($message);
+        $response = new Response($message, 400, ["Content-Type" => "application/json"]);
 
         // HttpExceptionInterface is a special type of exception that
         // holds status code and header details
